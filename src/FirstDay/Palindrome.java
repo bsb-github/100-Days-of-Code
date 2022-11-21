b1529a391195b27f0b5828f3b39f6cff80014fbb
bsb-github <codewithsheri64@gmail.com>
2022-11-21 13:22:16 +0500

    1st Day

--- src/FirstDay/Palindrome.java
+++ src/FirstDay/Palindrome.java
@@ -2,6 +2,6 @@ package FirstDay;
 
 public class Palindrome {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Hehe Palindrome Progran");
     }
 }
